ef20802cf140ff19189217f234ed0a3c7bfab220
zlarbals <zlarbals@gmail.com>
2023-05-10 00:39:27 +0900

    update: DailyWeight생성 컨트롤러 파라미터화

--- src/main/java/com/zlarbals/weightscheduler/controller/DailyWeightController.java
+++ src/main/java/com/zlarbals/weightscheduler/controller/DailyWeightController.java
@@ -1,12 +1,8 @@
 package com.zlarbals.weightscheduler.controller;
 
-import com.zlarbals.weightscheduler.dto.RegisterWeightDto;
 import com.zlarbals.weightscheduler.service.DailyWeightService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
@@ -17,10 +13,10 @@ public class DailyWeightController {
 
     private final DailyWeightService dailyWeightService;
 
-    @GetMapping("/batch/nextmonth")
-    public void createNextMonthDailyWeight(){
-        LocalDate nextMonthDate = LocalDate.now().plusMonths(1);
-        dailyWeightService.createDailyWeight(nextMonthDate);
+    @PostMapping("")
+    public void createNextMonthDailyWeight(@RequestParam int year,@RequestParam int month){
+        LocalDate targetDate = LocalDate.of(year,month,1);
+        dailyWeightService.createDailyWeight(targetDate);
     }
 
 }
